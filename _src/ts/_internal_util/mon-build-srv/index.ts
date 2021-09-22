9581c87a2f93d83cdf34542aefd69d82108d76d8
Mischback <dev@mischback.de>
2021-09-22 20:39:39 +0200

    Added config option nodemonConf

--- _src/ts/_internal_util/mon-build-srv/index.ts
+++ _src/ts/_internal_util/mon-build-srv/index.ts
@@ -1,10 +1,19 @@
 /* NodeJS modules */
 // import nodemon = require("nodemon");
+import fs = require("fs");
 import path = require("path");
 import stdio = require("stdio");
 
+const EXIT_CONFIG_FAILURE = 3;
+
 function main(): void {
   const options = stdio.getopt({
+    nodemonConf: {
+      description: "Use this config file for nodemon",
+      required: false,
+      args: 1,
+      default: "./nodemon.json",
+    },
     webRoot: {
       description: "Serve http from this directory",
       required: false,
@@ -13,8 +22,44 @@ function main(): void {
     },
   });
 
-  if (options !== null)
-    console.log("webRoot: " + path.resolve(options.webRoot.toString()));
+  const config = {
+    nodemonConf: "./nodemon.json",
+    webRoot: "./",
+  };
+
+  if (options !== null) {
+    try {
+      fs.accessSync(
+        path.resolve(options.nodemonConf.toString()),
+        fs.constants.R_OK
+      );
+      config.nodemonConf = path.resolve(options.nodemonConf.toString());
+    } catch (err) {
+      console.error("Could not read nodemon configuration!");
+      process.exit(EXIT_CONFIG_FAILURE);
+    }
+
+    try {
+      fs.accessSync(
+        path.resolve(options.webRoot.toString()),
+        fs.constants.R_OK
+      );
+      config.webRoot = path.resolve(options.webRoot.toString());
+    } catch (err) {
+      console.error("The specified webRoot could not be read!");
+      process.exit(EXIT_CONFIG_FAILURE);
+    }
+  }
+
+  try {
+    const buffer = fs.readFileSync(config.nodemonConf, "utf-8");
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const nodemon_conf = JSON.parse(buffer.toString());
+    console.log(nodemon_conf);
+  } catch (err) {
+    console.error("Could not parse nodemon configuration!");
+    process.exit(EXIT_CONFIG_FAILURE);
+  }
 }
 
 main();
