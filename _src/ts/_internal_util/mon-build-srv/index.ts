65c8ef55bc569397bca74eac39374776336f2155
Mischback <dev@mischback.de>
2021-09-22 19:42:31 +0200

    Added first config option webRoot

--- _src/ts/_internal_util/mon-build-srv/index.ts
+++ _src/ts/_internal_util/mon-build-srv/index.ts
@@ -1,8 +1,20 @@
 /* NodeJS modules */
 // import nodemon = require("nodemon");
+import path = require("path");
+import stdio = require("stdio");
 
 function main(): void {
-  console.log("mon-build-srv");
+  const options = stdio.getopt({
+    webRoot: {
+      description: "Serve http from this directory",
+      required: false,
+      args: 1,
+      default: "./",
+    },
+  });
+
+  if (options !== null)
+    console.log("webRoot: " + path.resolve(options.webRoot.toString()));
 }
 
 main();
