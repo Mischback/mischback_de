633ad8e160a99c87e3f11c34973f0f48d88b479d
Mischback <dev@mischback.de>
2021-09-24 19:49:00 +0200

    Completed actual response implementation

--- _src/ts/_internal_util/dev-bms/http-server.ts
+++ _src/ts/_internal_util/dev-bms/http-server.ts
@@ -7,6 +7,17 @@ import fs = require("fs");
 /* project files */
 import { DevBMSServerError, DevBMSRessourceNotFoundError } from "../errors";
 
+const mimeTypes: { [id: string]: string } = {
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".jpeg": "image/jpg",
+  ".gif": "image/gif",
+};
+
 function getUriFromRequest(request: http.IncomingMessage): Promise<string> {
   return new Promise((resolve, _reject) => {
     const url = request.url ? request.url : "/";
@@ -46,25 +57,42 @@ export function launchHttpServer(): Promise<void> {
     try {
       const port = 8000;
       const host = "0.0.0.0";
+      const webRoot = "_site";
 
       http
         .createServer(
           (request: http.IncomingMessage, response: http.ServerResponse) => {
             getUriFromRequest(request)
               .then((uri) => {
-                return determineRessourceFromUri(uri, "_site");
+                return determineRessourceFromUri(uri, webRoot);
               })
               .then((ressource) => {
-                console.log("[debug] determined this ressource: ", ressource);
+                const mime =
+                  mimeTypes[path.extname(ressource)] ||
+                  "application/octet-stream";
+
+                const fileStream = fs.createReadStream(ressource);
+
+                fileStream.on("start", () => {
+                  response.writeHead(200, { "Content-Type": mime });
+                });
+                fileStream.on("data", (chunk: any) => {
+                  response.write(chunk);
+                });
+                fileStream.on("end", () => {
+                  response.end();
+                });
+                fileStream.on("error", () => {
+                  return reject(new DevBMSServerError("foo"));
+                });
               })
               .catch((err) => {
                 if (err instanceof DevBMSRessourceNotFoundError)
+                  /* TODO: Continue here and provide 404 */
                   console.log("Requested ressource not found!");
 
                 console.log(err);
               });
-
-            response.end("Hello World");
           }
         )
         .listen(port, host);
