6be6fdcd1e457639b03f463d8880d30eed0b8efa
Mischback <dev@mischback.de>
2021-09-29 08:39:14 +0200

    Integrated logging and implemented debug mode

--- _src/ts/_internal_util/image-processor/index.ts
+++ _src/ts/_internal_util/image-processor/index.ts
@@ -2,10 +2,11 @@
 import { getopt } from "stdio";
 import { Logger } from "tslog";
 
+/* setting up the logger */
 const logger = new Logger({
   name: "image-processor",
   displayLoggerName: true,
-  minLevel: "debug",
+  minLevel: "info",
   dateTimePattern: "hour:minute:second",
   dateTimeTimezone: "Europe/Berlin",
   displayFunctionName: false,
@@ -16,13 +17,29 @@ function main(): void {
   const options = getopt({
     configFile: {
       description: "path/filename of the config file",
+      key: "c",
       required: false,
       args: 1,
       default: "image-processor.json",
     },
+    debug: {
+      description: "Activate debug mode",
+      key: "d",
+      required: false,
+      default: false,
+    },
   });
 
   if (options !== null) {
+    if (options.debug === true) {
+      logger.info("Debug mode activated!");
+      logger.setSettings({
+        minLevel: "debug",
+        displayFunctionName: true,
+        displayFilePath: "hideNodeModulesOnly",
+      });
+    }
+
     logger.debug("Config: " + options.configFile.toString());
   }
 }
