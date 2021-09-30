e735740f06ce111b919cce92482758d5413e7dec
Mischback <dev@mischback.de>
2021-09-30 13:47:47 +0200

    Next try, providing better code structure

--- _src/ts/_internal_util/image-processor/index.ts
+++ _src/ts/_internal_util/image-processor/index.ts
@@ -1,61 +1,14 @@
 /* NodeJS imports */
 import { createReadStream, readFileSync } from "fs";
 import { basename, extname, join } from "path";
-import sharp = require("sharp");
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
-const sharpInstance = require("sharp");
 import { getopt } from "stdio";
 import { Logger } from "tslog";
 
+/// <reference types="sharp" />
+import sharp = require("sharp");
+
 const EXIT_SUCCESS = 0;
 const EXIT_IMAGE_PROCESSOR_FAILURE = 6;
-
-interface extensionsDict {
-  [index: string]: string;
-}
-
-interface ImageProcessorConfigTargetTypes {
-  [index: string]:
-    | sharp.JpegOptions
-    | sharp.WebpOptions
-    | sharp.AvifOptions
-    | sharp.HeifOptions
-    | sharp.GifOptions
-    | sharp.TiffOptions
-    | sharp.PngOptions
-    | undefined;
-  jpeg?: sharp.JpegOptions;
-  webp?: sharp.WebpOptions;
-  avif?: sharp.AvifOptions;
-  heif?: sharp.HeifOptions;
-  gif?: sharp.GifOptions;
-  tiff?: sharp.TiffOptions;
-  png?: sharp.PngOptions;
-}
-
-interface ImageProcessorConfigTargetDimensionsFull {
-  mode: "full" | "width" | "height";
-  width?: number;
-  height?: number;
-  filenameSuffix?: string;
-}
-
-interface ImageProcessorConfigTargetDimensions {
-  [index: string]: ImageProcessorConfigTargetDimensionsFull;
-}
-
-interface ImageProcessorConfig {
-  target_types: ImageProcessorConfigTargetTypes;
-  target_dimensions: ImageProcessorConfigTargetDimensions;
-}
-
-const fileExtensions: extensionsDict = {
-  gif: ".gif",
-  jpeg: ".jpg",
-  jpg: ".jpg",
-  png: ".png",
-  webp: ".webp",
-};
 
 /* setting up the logger */
 const logger = new Logger({
@@ -68,91 +21,81 @@ const logger = new Logger({
   displayFilePath: "hidden",
 });
 
-function sharpWrapper(
-  config: ImageProcessorConfig,
-  inputFile: string,
+function buildSharpPipes(
+  sharpPipeEntry: sharp.Sharp,
+  fileBasename: string,
   outputDir: string
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    const sharpStream = new sharpInstance({
-      failOnError: true,
-    }) as sharp.Sharp;
-
+): Promise<sharp.Sharp[]> {
+  return new Promise((resolve, _reject) => {
     const sharpPipes: sharp.Sharp[] = [];
+
+    let pipe = sharpPipeEntry.clone();
+
+    pipe = pipe.resize({ width: 200 });
+
+    let newFilename: string;
+    /* The next line is just for testing, will be removed in the real
+     * implementation anyway.
+     */
+    // eslint-disable-next-line no-constant-condition
+    if (1 === 1) {
+      newFilename = fileBasename + "-small" + ".jpg";
+    } else {
+      newFilename = fileBasename + ".jpg";
+    }
+
+    pipe = pipe.toFormat("jpeg");
+
+    /* The following line is ignored from TypeScript checks, because they
+     * find, that the Promise is not fully populated. Indeed, it will be
+     * fully populated, but TypeScript does not know this, because the call
+     * to "toFormat()" is performed with an variable filetype.
+     */
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    pipe = pipe.toFile(join(outputDir, newFilename));
+
+    logger.debug("Built pipe...");
+
+    sharpPipes.push(pipe);
+
+    return resolve(sharpPipes);
+  });
+}
+
+function sharpWrapper(inputFile: string, outputDir: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const sharpPipeEntry = sharp({
+      failOnError: true,
+    });
 
     const fileBasename = basename(inputFile, extname(inputFile));
 
-    Object.keys(config.target_types).forEach((t) => {
-      const filetype = t.toString().toLowerCase();
-      const filetype_options = config.target_types[t];
-      const filetype_extension = fileExtensions[filetype];
+    buildSharpPipes(sharpPipeEntry, fileBasename, outputDir)
+      .then((sharpPipes) => {
+        const readStream = createReadStream(inputFile);
+        readStream.on("open", () => {
+          readStream.pipe(sharpPipeEntry);
+        });
+        readStream.on("error", (err) => {
+          logger.error("Error while reading input stream:");
+          logger.fatal(err);
+          return reject("foo");
+        });
 
-      Object.keys(config.target_dimensions).forEach((d) => {
-        const dim = config.target_dimensions[d] || { mode: "full" };
-
-        let pipe = sharpStream.clone();
-
-        let newFilename: string;
-
-        if (dim.mode !== "full") {
-          if (dim.mode === "width") pipe = pipe.resize({ width: dim.width });
-          if (dim.mode === "height") pipe = pipe.resize({ height: dim.height });
-        }
-
-        if (dim.filenameSuffix !== undefined)
-          newFilename =
-            fileBasename + dim.filenameSuffix.toString() + filetype_extension;
-        else newFilename = fileBasename + filetype_extension;
-
-        pipe = pipe.toFormat(
-          filetype as keyof sharp.FormatEnum,
-          filetype_options
-        );
-
-        /* The following line is ignored from TypeScript checks, because they
-         * find, that the Promise is not fully populated. Indeed, it will be
-         * fully populated, but TypeScript does not know this, because the call
-         * to "toFormat()" is performed with an variable filetype.
-         */
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        pipe = pipe.toFile(join(outputDir, newFilename));
-
-        logger.debug(
-          'Built pipe to create "' +
-            newFilename +
-            '" from "' +
-            inputFile +
-            '"...'
-        );
-
-        sharpPipes.push(pipe);
-      });
-    });
-
-    const readStream = createReadStream(inputFile);
-    readStream.on("open", () => {
-      logger.debug('Opened "' + inputFile + '" and piping into Sharp...');
-      readStream.pipe(sharpStream);
-    });
-    readStream.on("error", (err) => {
-      logger.error("Error while reading input stream:");
-      logger.fatal(err);
-      return reject("foo");
-    });
-
-    Promise.all(sharpPipes)
-      .then(() => {
-        logger.debug(
-          'Processed all pipes for source file "' + inputFile + '"...'
-        );
-        return resolve();
+        Promise.all(sharpPipes)
+          .then(() => {
+            logger.debug("Processed all pipes...");
+            return resolve();
+          })
+          .catch((err) => {
+            logger.error("Error while processing the pipes...");
+            logger.fatal(err);
+            return reject("foo");
+          });
       })
-      .catch((err) => {
-        logger.error("Error while processing the pipes...");
-        logger.fatal(err);
-        return reject("foo");
+      .catch(() => {
+        return reject();
       });
   });
 }
@@ -205,23 +148,15 @@ function main(): void {
     const config = JSON.parse(
       readFileSync(options.configFile.toString(), "utf-8")
     );
+    logger.debug(config);
 
-    sharpWrapper(
-      config,
-      options.inputFile.toString(),
-      options.outputDir.toString()
-    )
+    sharpWrapper(options.inputFile.toString(), options.outputDir.toString())
       .then(() => {
-        logger.info(
-          'Finished processing of input file "' +
-            options.inputFile.toString() +
-            '"!'
-        );
+        logger.info("Successfully processed image!");
         process.exit(EXIT_SUCCESS);
       })
-      .catch((err) => {
-        logger.fatal("Something went terribly wrong!");
-        logger.trace(err);
+      .catch(() => {
+        logger.error("Error while processing the image!");
         process.exit(EXIT_IMAGE_PROCESSOR_FAILURE);
       });
   }
